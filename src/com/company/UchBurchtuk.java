3ec1ac9d6f4870305baea08d228622f02249f391
azamat <azamat.kasymaliev@bk.ru>
2022-02-02 17:40:13 +0600

    this is comments

--- src/com/company/UchBurchtuk.java
+++ src/com/company/UchBurchtuk.java
@@ -1,9 +1,20 @@
 package com.company;
 
 public class UchBurchtuk {
+
     int a;
     int b;
     int c;
+
+
+    public void area() {
+
+        System.out.println(a + b + c+ " =Total Area");
+
+
+
+
+    }
 
 
 }
